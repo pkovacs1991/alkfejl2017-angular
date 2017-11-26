b8b8355c3b4c9abe6b7ec6455842a4877549070b
pkovacs1991 <p.kovacs1991@gmail.com>
2017-11-26 10:16:46 +0100

    Delete app folder

--- app/models/category.ts
+++
@@ -1,7 +0,0 @@
-import {Recipe} from "./recipe";
-
-export class Category {
-    id: number;
-    name: string;
-    recipe?: Recipe[];
-}
--- app/models/comment.ts
+++
@@ -1,9 +0,0 @@
-import {Recipe} from "./recipe";
-import {User} from "./user";
-
-export class Comment {
-    id: number;
-    user: User;
-    recipe: Recipe;
-    comment: string;
-}
